446b0359a057d248bb91b6c9b10ae81c678edf1e
invmy <invmy@outlook.com>
2025-04-21 20:46:14 +0800

    index pages

---
+++ src/data/info.ts
@@ -0,0 +1,22 @@
+export const info = {
+  baseUrl: "https://OpenPriceAction.github.com/",
+  name: "Open Price Action",
+  jobDescription: "trading",
+  about: `Trading Beter,  Best Trading`,
+
+  projects: [
+    {
+      title: "blog",
+      isFeatured: true,
+      thumbnail: "/assets/images/blog.avif",
+      githubUrl: "https://github.com/OpenPriceAction/blog",
+      liveUrl: "https://OpenPriceAction.github.com/blog",
+    },    {
+      title: "docs",
+      isFeatured: true,
+      thumbnail: "/assets/images/doc.avif",
+      githubUrl: "https://github.com/OpenPriceAction/docs",
+      liveUrl: "https://OpenPriceAction.github.com/docs",
+    }
+  ],
+};
